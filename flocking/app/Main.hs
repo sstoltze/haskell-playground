35d5237b19f25145005be1290dcad816e7883446
Sarah Stoltze <sstoltze@gmail.com>
2021-11-10 21:45:33 +0100

    Allow setting generated image directory

--- flocking/app/Main.hs
+++ flocking/app/Main.hs
@@ -6,6 +6,7 @@ import           Flock
 import           Space
 
 import           Data.Functor       ((<&>))
+import           Data.Maybe         (listToMaybe)
 import           System.Environment (getArgs)
 import           System.Random
 
@@ -13,6 +14,7 @@ data AppConfig = AppConfig { numberOfRuns     :: Int
                            , numberOfEntities :: Int
                            , pictureWidth     :: Int
                            , pictureHeight    :: Int
+                           , imageDirectory   :: String
                            }
 
 defaultAppConfig :: AppConfig
@@ -20,6 +22,7 @@ defaultAppConfig = AppConfig { numberOfRuns = 50
                              , numberOfEntities = 20
                              , pictureWidth = 500
                              , pictureHeight = 500
+                             , imageDirectory = "/tmp/"
                              }
 
 flockDraw :: AppConfig -> Flock -> DynamicImage
@@ -34,6 +37,15 @@ flockDraw config f = ImageRGB8 (generateImage draw width height)
          then PixelRGB8 200 200 200
          else PixelRGB8 50 50 50
 
+buildFileName :: AppConfig -> Int -> String
+buildFileName AppConfig { imageDirectory = dir } k = correctDir ++ "flock" ++ show k ++ ".png"
+  where
+    correctDir = case safeLast dir of
+      Just '/'  -> dir
+      Just '\\' -> dir
+      _         -> dir ++ "/"
+    safeLast = listToMaybe . reverse
+
 flockRun :: AppConfig -> Flock -> IO Flock
 flockRun config = flockRun' 0
   where
@@ -42,7 +54,7 @@ flockRun config = flockRun' 0
           if k == iterations
           then return f
           else do
-            let fileName = "/tmp/flock" ++ show k ++ ".png"
+            let fileName = buildFileName config k
             savePngImage fileName (flockDraw config f)
             putStrLn fileName
             flockRun' (k+1) (flockUpdate f)
@@ -68,4 +80,5 @@ parseArgs as = parseArgs' as defaultAppConfig
     parseArgs' ("-h":n:as') config = parseArgs' as' $ config { pictureHeight = read n }
     parseArgs' ("--width":n:as') config = parseArgs' as' $ config { pictureWidth = read n }
     parseArgs' ("-w":n:as') config = parseArgs' as' $ config { pictureWidth = read n }
+    parseArgs' ("--output-dir":d:as') config = parseArgs' as' $ config { imageDirectory = d }
     parseArgs' _ config = config
