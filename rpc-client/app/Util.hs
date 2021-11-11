a3091c62d38c24bab5d0f316219d76e6bde39de0
Sarah Stoltze <sstoltze@gmail.com>
2021-11-11 23:43:44 +0100

    Make it work on windows (maybe break linux)

--- rpc-client/app/Util.hs
+++ rpc-client/app/Util.hs
@@ -1,8 +1,11 @@
+{-# LANGUAGE CPP #-}
 module Util where
 
 import           Control.Concurrent
 import           Data.Functor
+#ifndef mingw32_HOST_OS
 import           System.Posix.Signals
+#endif
 
 data Results = Results { resultsSuccess :: Int
                        , resultsFailure :: Int
@@ -26,9 +29,12 @@ putResult results resp = modifyMVar_ results $ return .
 printResults :: MVar Results -> IO ()
 printResults r = readMVar r >>= print
 
--- Handle C-c by killing all running threads
-installKillHandler :: [ThreadId] -> IO ()
-installKillHandler threadIds = void $ installHandler keyboardSignal (Catch (mapM_ killThread threadIds)) Nothing
-
 oneSecond :: Int
 oneSecond = 1000000
+
+installKillHandler :: [ThreadId] -> IO ()
+#ifdef mingw32_HOST_OS
+installKillHandler = return . const ()
+#else
+installKillHandler threadIds = void $ installHandler keyboardSignal (Catch (mapM_ killThread threadIds)) Nothing
+#endif
